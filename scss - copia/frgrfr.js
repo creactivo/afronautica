19e32bb2327505f072bc0f3035d060cbd3fe5536
Creactivo <zonacreactiva@gmail.com>
2018-06-21 19:57:00 -0500

    99-2%

---
+++ scss - copia/frgrfr.js
@@ -0,0 +1,52 @@
+
+$($class4b).on('click',function(){
+   var ff = document.querySelector("show");
+   var a = document.querySelector(".boton1a");
+   var b = document.querySelector(".boton2a");
+   var c = document.querySelector(".boton3a");
+   var d = document.querySelector(".boton4a");
+   var e = document.querySelector(".boton5a");
+   var f = document.querySelector(".boton5");
+   var g = document.querySelector(".boton6");
+   var h = document.querySelector(".boton9");
+   var i = document.querySelector(".boton10");
+   var j = document.querySelector(".boton13");
+   var k = document.querySelector(".boton14");
+   var l = document.querySelector(".boton17");
+ 
+   if(ff == null){
+    $("#boton-menu3").addClass('blue')
+    }
+    else if(a != null){
+      $("#boton-menu3").addClass('blue')
+    }else if(b != null){
+      $("#boton-menu3").addClass('blue')
+    }else if(c != null){
+      $("#boton-menu3").addClass('blue')
+    }else if(d != null){
+      $("#boton-menu3").addClass('blue')
+    }else if(e != null){
+      $("#boton-menu3").addClass('blue')
+    }else if(f != null){
+      $("#boton-menu3").addClass('blue')
+    }else if(g != null){
+      $("#boton-menu3").addClass('blue')
+    }else if(h != null){
+      $("#boton-menu3").addClass('blue')
+    }else if(i != null){
+      $("#boton-menu3").addClass('blue')
+    }else if(j != null){
+      $("#boton-menu3").addClass('blue')
+    }else if(k != null){
+      $("#boton-menu3").addClass('blue')
+    }else if(l != null){
+      $("#boton-menu3").addClass('blue')
+    }
+    else{
+      $("#boton-menu3").removeClass('blue')
+    }
+    
+    $class5c.toggleClass('.bg-light');
+});
+ // Elimina las animacion de los
+ //contenedores que aprecen horizontalmente
